7759d292cfa62e1de97bb770dc282bd2923054d9
olena4reunited <olenaduringintermission@gmail.com>
2024-10-11 15:33:19 +0300

    build: airport base serializer, viewset and url

--- airport/urls.py
+++ airport/urls.py
@@ -1,3 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from airport.views import AirportViewSet
+
+router = DefaultRouter()
+
+router.register("airports", AirportViewSet, basename="airport")
+
 urlpatterns = []
 
 app_name = "airport"
--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import Airport
+from airport.serializers import AirportSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer

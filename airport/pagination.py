26f6fec3c35832e8a3e4c4eec579761d990c742f
olena4reunited <olenaduringintermission@gmail.com>
2024-11-06 03:56:00 +0200

    fix: set DEFAULT_PAGINATION_CLASS for the whole project and remove unused template dirs

--- airport/pagination.py
+++ airport/pagination.py
@@ -6,13 +6,3 @@ class CustomPagination(PageNumberPagination):
     page_size = 4
     page_size_query_param = "page_size"
     max_page_size = 10
-
-    def get_paginated_response(self, data):
-        return Response(
-            {
-                "count": self.page.paginator.count,
-                "next": self.get_next_link(),
-                "previous": self.get_previous_link(),
-                "results": data,
-            }
-        )
